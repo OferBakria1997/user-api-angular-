f1e8fcadf6af8bbbe6b30428d876d53c1b98e0aa
ofer bakria <you@example.com>
2020-10-16 16:16:15 +0300

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
+
 const express = require('express');
-const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/gallery'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/gallery/index.html'));
-});
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static('./dist/frontend-conference-w-angular'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/frontend-conference-w-angular/'}
+  );
+  });
+
+  app.listen(process.env.PORT || 8080);
