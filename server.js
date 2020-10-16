ada96b9bf57f378172f5fcf2e637dd1a06220f22
ofer bakria <you@example.com>
2020-10-16 16:22:32 +0300

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/frontend-conference-w-angular'));
+app.use(express.static('./dist/gallery'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/frontend-conference-w-angular/'}
+    res.sendFile('index.html', {root: 'dist/gallery/'}
   );
   });
 
